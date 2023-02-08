44031eb43f4abecd6186da76277315fc0659f6de
James Grant <jamespgrant3@gmail.com>
2023-02-08 09:17:11 -0500

    updated api to call user-api

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService, User } from './app.service';
 
-@Controller()
+@Controller('users')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getUsers(): Promise<User[]> {
+    return this.appService.getUsers();
   }
 }
--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
+const userApiUrl = process.env.USER_API_URL || '';
+
+export interface User {
+  firstName: string;
+  lastName: string;
+}
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  public async getUsers(): Promise<User[]> {
+    const response = await fetch(`${userApiUrl}/users`, { method: 'GET' });
+    return response.json();
   }
 }
