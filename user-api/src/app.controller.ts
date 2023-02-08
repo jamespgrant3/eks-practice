1ad4628d46a563640c84a5586d204952769360d4
James Grant <jamespgrant3@gmail.com>
2023-02-08 08:08:53 -0500

    updated app controller to return a random number of users

--- user-api/src/app.controller.ts
+++ user-api/src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService, User} from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('users')
+  getHello(): User[]{
+    return this.appService.getUsers();
   }
 }
